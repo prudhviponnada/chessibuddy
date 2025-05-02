c75cde6180ea4bdccfa6366f2e35efab03462293
ponnadaprudhvi <ponnadaprudhvi18@gmail.com>
2025-05-02 09:25:57 +0200

    modified:   chessy.py 	modified:   feedback.txt

--- chessy.py
+++ chessy.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 import chess
 import random
 import os
@@ -285,4 +285,4 @@ def feedback():
         return jsonify({'error': 'Error saving feedback.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port="5000",debug=True)
